67d7efafadb90e6c730972b6ddcc12810135d8b6
Nitesh <imnksid@gmail.com>
2026-04-12 21:40:01 +0530

    Added icons and images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,55 @@
+import apple_earphone_image from "./apple_earphone_image.png";
+import asus_laptop_image from "./asus_laptop_image.png";
+import bose_headphone_image from "./bose_headphone_image.png";
+import boy_with_laptop_image from "./boy_with_laptop_image.png";
+import cannon_camera_image from "./cannon_camera_image.png";
+import checkmark_image from "./checkmark_image.png";
+import girl_with_earphone_image from "./girl_with_earphone_image.png";
+import girl_with_headphone_image from "./girl_with_headphone_image.png";
+import header_headphone_image from "./header_headphone_image.png";
+import header_macbook_image from "./header_macbook_image.png";
+import header_playstation_image from "./header_playstation_image.png";
+import jbl_soundbox_image from "./jbl_soundbox_image.png";
+import macbook_image from "./macbook_image.png";
+import md_controller_image from "./md_controller_image.png";
+import playstation_image from "./playstation_image.png";
+import product_details_page_apple_earphone_image1 from "./product_details_page_apple_earphone_image1.png";
+import product_details_page_apple_earphone_image2 from "./product_details_page_apple_earphone_image2.png";
+import product_details_page_apple_earphone_image3 from "./product_details_page_apple_earphone_image3.png";
+import product_details_page_apple_earphone_image4 from "./product_details_page_apple_earphone_image4.png";
+import product_details_page_apple_earphone_image5 from "./product_details_page_apple_earphone_image5.png";
+import projector_image from "./projector_image.png";
+import samsung_s23phone_image from "./samsung_s23phone_image.png";
+import sm_controller_image from "./sm_controller_image.png";
+import sony_airbuds_image from "./sony_airbuds_image.png";
+import upload_area_image from "./upload_area_image.png";
+import venu_watch_image from "./venu_watch_image.png";
+
+export const IMAGES = {
+  apple_earphone_image,
+  asus_laptop_image,
+  bose_headphone_image,
+  boy_with_laptop_image,
+  cannon_camera_image,
+  checkmark_image,
+  girl_with_earphone_image,
+  girl_with_headphone_image,
+  header_headphone_image,
+  header_macbook_image,
+  header_playstation_image,
+  jbl_soundbox_image,
+  macbook_image,
+  md_controller_image,
+  playstation_image,
+  product_details_page_apple_earphone_image1,
+  product_details_page_apple_earphone_image2,
+  product_details_page_apple_earphone_image3,
+  product_details_page_apple_earphone_image4,
+  product_details_page_apple_earphone_image5,
+  projector_image,
+  samsung_s23phone_image,
+  sm_controller_image,
+  sony_airbuds_image,
+  upload_area_image,
+  venu_watch_image,
+};
