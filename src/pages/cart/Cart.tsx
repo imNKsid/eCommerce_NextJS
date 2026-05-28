29aa77e832f9b1a532d2d14cb6c581863307842b
Nitesh <imnksid@gmail.com>
2026-05-29 00:49:57 +0530

    Cart page created with CartItems component

---
+++ src/pages/cart/Cart.tsx
@@ -0,0 +1,12 @@
+import CartItems from '@/components/cart-items/CartItems'
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <CartItems />
+    </div>
+  )
+}
+
+export default Cart
